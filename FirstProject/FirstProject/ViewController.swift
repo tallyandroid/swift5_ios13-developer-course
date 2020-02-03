0779c9ed52fee9eedd36b03025aedab5450e5886
Christian Tisby <cduane82@hotmail.com>
2020-02-03 02:47:32 -0500

    first commit

---
+++ FirstProject/FirstProject/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  FirstProject
+//
+//  Created by Christian Tisby on 2/3/20.
+//  Copyright © 2020 Christian Tisby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func btnClick(_ sender: Any) {
+        print("Clicked")
+    }
+}
+
